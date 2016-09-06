b3064bc6f3b86e80843b97731b30899f28e0d939
Kevin Brosamle <kevin@Kevins-MacBook-Pro-2.local>
2016-09-06 15:24:10 -0700

    Refactored to modules between server, api, and routes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var request = require('request');
+var indeed = require('./indeedApi');
 
 var app = express();
 
+require('./routes.js')(app, express);
+
 app.listen(3000, function() {
-  console.log('this is working on 3000');
+  console.log('Connected on port 3000');
 });
 
 app.get('/', function(req, res) {
-  res.send('this is a test');
+  res.send('INDEX PAGE');
 });
+
+module.exports = app;
