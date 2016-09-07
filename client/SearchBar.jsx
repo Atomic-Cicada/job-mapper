7568bcf29e4c213d316e06622c9005d83f1a16fc
Aljosha Novakovic <ollynov@gmail.com>
2016-09-06 22:06:29 -0700

    Successfully handles onSubmit function

--- client/SearchBar.jsx
+++ client/SearchBar.jsx
@@ -1,21 +1,46 @@
 import React, {PropTypes, Component} from 'react';
 
 export default class SearchBar extends Component {
+  // static defaultProps = {
+
+  // };
+
   constructor(props) {
     super(props);
+    this.state= {
+      currentJob: '',
+      currentCity: ''
+    };
+
+    this.handleJobSearch = this.handleJobSearch.bind(this);
+    this.handleCitySearch = this.handleCitySearch.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('currentCity is ', this.state.currentCity)
+    console.log('currentJob is ', this.state.currentJob)
+  }
+
+  handleJobSearch(e) {
+    this.setState({currentJob: e.target.value});
+    console.log(this)
+  }
+  handleCitySearch(e) {
+    this.setState({currentCity: e.target.value});
+    console.log(this)
   }
 
   render() {
     return (
       <div id='SearchBar'>
-        <form>
-          <div className='searchLabel'>Job Title:<input type="text" name="job"/></div>
-          <div className='searchLabel'>City:<input type="text" name="city"/></div>
+        <form onSubmit={this.handleSubmit}>
+          <div className='searchLabel'>Job Title:<input type="text" name="job" value={this.state.currentJob} onChange={this.handleJobSearch}/></div>
+          <div className='searchLabel'>City:<input type="text" name="city" value={this.state.currentCity} onChange={this.handleCitySearch} /></div>
           <input type="submit" name="submit"/>
         </form>
       </div>
     );
   }
 }
-
-
