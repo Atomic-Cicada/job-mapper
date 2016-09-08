c37d742afe07285ca26ebb74e8f48338dc3095f4
Andrew <andrewk17@gmail.com>
2016-09-08 16:57:37 -0700

    Fix requires after moving files around

---
+++ server/database/users/userschema.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let mongoose = require('mongoose');
+
+let userSchema = mongoose.Schema({
+  username: {type: String, unique: true},
+  password: {type: String},
+  salt: {type: String}
+});
+ 
+let User = mongoose.model('User', userSchema);
+
+module.exports = User;
