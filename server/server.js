fe575f1ca3800dc7d16ccba4018350f1431c6bde
Aljosha Novakovic <ollynov@gmail.com>
2016-09-08 14:17:05 -0700

    Started bare bones authentication

--- server/server.js
+++ server/server.js
@@ -19,3 +19,36 @@ app.listen(3000, () => {
 });
 
 module.exports = app;
+
+
+// AUTHENTICATION
+// Still needed:
+// - login component
+// - profile component
+
+var session = require('express-session');
+
+var restrict = function(req, res, next) {
+  if (req.session.login) {
+    next();
+  } else {
+    //route user to login view
+    console.log('you are being redirected to login');
+    res.redirect(301, '/login');
+  }
+};
+
+
+// ROUTES
+// A user can only access his profile if he is logged in.
+app.get('/profile', restrict,
+  function(req, res) {
+    res.render('index');
+  });
+
+// Signup
+app.get('/signup',
+  function(req, res) {
+    res.render('signup');
+  });
+
