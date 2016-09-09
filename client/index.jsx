4531afb95520fe8eef119f32ff4adeedb3df3ad5
Aljosha Novakovic <ollynov@gmail.com>
2016-09-09 15:14:07 -0700

    Use ES6 and all of Andrew's refactoring suggestions

--- client/index.jsx
+++ client/index.jsx
@@ -2,5 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import Container from './Container/Container.jsx';
 require('./main.css');
+// import Filter from './FilterBar.jsx';
+
 
 render(<Container/>, document.getElementById('app'));
