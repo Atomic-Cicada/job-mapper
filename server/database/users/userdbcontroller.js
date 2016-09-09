a086aa91d72e385721a13b1318fea6c54aa9a138
Aljosha Novakovic <ollynov@gmail.com>
2016-09-09 09:22:55 -0700

    Correctly saving hashed passwords and salt

--- server/database/users/userdbcontroller.js
+++ server/database/users/userdbcontroller.js
@@ -1,6 +1,9 @@
 'use strict';
 
 let User = require('./userschema.js');
+var bcrypt = require('bcryptjs');
+
+
 
 module.exports = {
   // retrieveAll: (req, res) => {
@@ -9,26 +12,35 @@ module.exports = {
   //   });
   // }
   addOne: (req, res) => {
-    var user = new User({
-      username: req.body.currentUsername,
-      password: req.body.currentPassword,
-      salt: 'tempsalt'
-    });
-    User.count({ username: req.body.currentUsername }, function (err, count) {
-      // In other words, if the username is already taken
-      // Count is faster than the find() function
-      if (count > 0) {
-        res.status(300).send('Sorry username already taken!!');
-      } else {
-        user.save(function(err) {
-          if (err) {
-            return console.error('here is the error', err);
-          }
-        }).then((results) => {
-          res.status(200).send(JSON.stringify(results));
+    //var salt = bcrypt.genSaltSync(10);
+    //var hash = bcrypt.hashSync(req.body.currentPassword, salt);
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(req.body.currentPassword, salt, function(err, hash) {
+        // Store hash in your password DB.
+        var user = new User({
+          username: req.body.currentUsername,
+          password: hash,
+          salt: salt
         });
-      }
+
+        User.count({ username: req.body.currentUsername }, function (err, count) {
+        // In other words, if the username is already taken
+        // Count is faster than the find() function
+          if (count > 0) {
+            res.status(300).send('Sorry username already taken!!');
+          } else {
+            user.save(function(err) {
+              if (err) {
+                return console.error('here is the error', err);
+              }
+            }).then((results) => {
+              res.status(200).send(JSON.stringify(results));
+            });
+          }
+        });
+      });
     });
+
   }
 
 
