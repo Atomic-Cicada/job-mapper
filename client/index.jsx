7a6a7c16d993adca1fa179625d1b6b0d610a3eb8
Aljosha Novakovic <ollynov@gmail.com>
2016-09-12 14:15:24 -0700

    Reflect Kevin's comments and suggested edits

--- client/index.jsx
+++ client/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {render} from 'react-dom';
 import Container from './Container/Container.jsx';
 require('./main.css');
-// import Filter from './FilterBar.jsx';
 
 
 render(<Container/>, document.getElementById('app'));
--- server/database/index.js
+++ server/database/index.js
@@ -1,8 +1,7 @@
 'use strict';
 
 let mongoose = require('mongoose');
-// let mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/jobmapper';
-let mongoUri = 'mongodb://localhost/jobmapper';
+let mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/jobmapper';
 
 let db = mongoose.connect(mongoUri);
 
